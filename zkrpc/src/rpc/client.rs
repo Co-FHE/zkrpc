af614db6d92587702747f34112c23b0cb4a4a89e
shiotoli <shiotoli@gmail.com>
2024-04-23 16:35:26 +0800

    simple rpc

--- zkrpc/src/rpc/client.rs
+++ zkrpc/src/rpc/client.rs
@@ -1,48 +1,55 @@
-use hello_world::greeter_client::GreeterClient;
-use hello_world::HelloRequest;
-
 pub mod pb {
     tonic::include_proto!("grpc.zkrpc.service");
 }
 #[cfg(test)]
 mod tests {
+    use crate::rpc::zkrpc;
+
     use super::*;
     use pb::zk_service_client::ZkServiceClient;
     use pb::ZkRequest;
     use tokio::runtime::Runtime;
-    use tonic::transport::Channel;
 
     #[test]
     fn test_zk_rpc() {
-        // Create a runtime for the async client
         let rt = Runtime::new().unwrap();
 
         rt.block_on(async {
-            // Set the address for the service
-            let addr = "http://[::1]:50051".parse().unwrap();
+            // Start the server
+            let _server_handle = tokio::spawn(async { zkrpc::ZkRpcServer::start().await.unwrap() });
 
-            // Create the client
+            let addr: &str = "http://[::1]:12345";
             let mut client = ZkServiceClient::connect(addr).await.unwrap();
-
-            // Create a request
+            // define mock request
+            let address_mock = "0x123456";
+            let block_height_mock = 1;
+            let epoch_mock = 2;
             let request = tonic::Request::new(ZkRequest {
-                // populate fields as needed
+                address: address_mock.into(),
+                block_height: block_height_mock,
+                epoch: epoch_mock,
             });
-
-            // Send the request
             let response = client.get_proof(request).await;
-
-            // Verify the response
             assert!(response.is_ok(), "Expected Ok response, got {:?}", response);
 
-            let unwrapped_response = response.unwrap();
+            let unwrapped_response = response.unwrap().into_inner();
             assert!(
-                !unwrapped_response.into_inner().alpha_proof.is_empty(),
-                "Expected non-empty alpha proof"
+                unwrapped_response.address == address_mock,
+                "Expected address to be {}, got {}",
+                address_mock,
+                unwrapped_response.address
             );
             assert!(
-                !unwrapped_response.into_inner().beta_proof.is_empty(),
-                "Expected non-empty beta proof"
+                unwrapped_response.block_height == block_height_mock,
+                "Expected block height to be {}, got {}",
+                block_height_mock,
+                unwrapped_response.block_height
+            );
+            assert!(
+                unwrapped_response.epoch == epoch_mock,
+                "Expected epoch to be {}, got {}",
+                epoch_mock,
+                unwrapped_response.epoch
             );
         });
     }
--- zkrpc/src/rpc/mod.rs
+++ zkrpc/src/rpc/mod.rs
@@ -1,3 +1,3 @@
+mod client;
 mod zkrpc;
-
 pub use zkrpc::*;
