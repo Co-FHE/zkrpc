8000c276470e66dcefde5363f9c0693d13b81279
shiotoli <shiotoli@gmail.com>
2024-04-25 20:36:55 +0800

    add satellites

--- pox/src/lib.rs
+++ pox/src/lib.rs
@@ -1,1 +1,19 @@
+use num_bigint::BigInt;
+use types::Satellite;
 
+pub struct PoX {
+    satellites: Vec<Satellite<BigInt>>,
+}
+impl PoX {
+    fn empty() -> Self {
+        Self {
+            satellites: Vec::new(),
+        }
+    }
+    fn new(satellites: Vec<Satellite<BigInt>>) -> Self {
+        Self { satellites }
+    }
+    fn add(&mut self, satellite: Satellite<BigInt>) {
+        self.satellites.push(satellite)
+    }
+}
